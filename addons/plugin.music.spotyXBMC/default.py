20f22882ad1bf34dd8320ce0e51665faf4b5b89f
akezeke <david.erenger@gmail.com>
2011-08-24 10:42:12 +0200

    Enable library without any local music, make the addon a little more informative

--- addons/plugin.music.spotyXBMC/default.py
+++ addons/plugin.music.spotyXBMC/default.py
@@ -8,3 +8,14 @@ __url__ = "https://github.com/akezeke/xbmc"
 __settings__ = xbmcaddon.Addon(id='plugin.music.spotyXBMC')
 __language__ = __settings__.getLocalizedString
 
+def startPlugin():
+	if (__settings__.getSetting("enable") == "false"):
+		__settings__.openSettings()
+	else:
+		dialog = xbmcgui.Dialog()
+		dialog.ok("spotyXBMC2", "Running the addon will not lead you to spotify music", "instead check out the regular secions in the", "music library!")
+
+	#TODO why is this leading to a list without playlists and search? Find a better way to do this
+	xbmc.executebuiltin('XBMC.ActivateWindow(music,musicdb://,Return)')		
+
+startPlugin()
